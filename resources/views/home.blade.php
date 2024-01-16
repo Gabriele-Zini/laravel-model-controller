7de23ede01694cdbbd8249b0d734231dcbf7ea8b
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-01-16 10:05:33 +0100

    style: changed row

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,16 @@
 
 @section('content')
     <div class="container my-5">
-        <div class="row justify-content-center gap-5">
+        <div class="row justify-content-start g-4">
             @foreach ($movies as $movie)
-                <div class="card col col-12 col-md-5 col-lg-3 p-0">
-                    <img src="{{ $movie->image }}" class="card-img-top p-0" alt="...">
-                    <div class="card-body ms_card-body">
-                        <p class="card-text"><span>Title: </span>{{ $movie->original_title }}</p>
-                        <p class="card-text"><span>Rating: </span>{{ floor($movie->vote) }}</p>
-                        <p class="card-text"><span>Nationality: </span>{{ $movie->nationality }}</p>
+                <div class="col col-12 col-md-5 col-lg-3 ">
+                    <div class="card p-0">
+                        <img src="{{ $movie->image }}" class="card-img-top p-0" alt="...">
+                        <div class="card-body ms_card-body">
+                            <p class="card-text"><span>Title: </span>{{ $movie->original_title }}</p>
+                            <p class="card-text"><span>Rating: </span>{{ floor($movie->vote) }}</p>
+                            <p class="card-text"><span>Nationality: </span>{{ $movie->nationality }}</p>
+                        </div>
                     </div>
                 </div>
             @endforeach
